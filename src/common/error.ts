09926e95ca10df08e39befd818832a52284f1748
Nishint Goyal <nishint.goyal@nishintgoyalvrs-17.successivetech.com>
2025-08-21 15:48:59 +0530

    Remove error class

--- src/common/error.ts
+++
@@ -1,8 +0,0 @@
-class CustomError extends Error {
-  constructor(message, code) {
-    super(message);
-    this.extensions = { code };  // GraphQL response me dikh jaayega
-  }
-}
-
-export default CustomError;
