74b1270ca53a046b6ba4a0d8959968531791b9bb
Nishint Goyal <nishint.goyal@nishintgoyalvrs-17.successivetech.com>
2025-08-22 16:50:02 +0530

    Added 2 Questions of Assign-3

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,58 @@
-import { ApolloServer } from "apollo-server";
+// import { ApolloServer } from "apollo-server";
+// import schema from "./graphql/schema.js";
+
+// async function startServer() {
+//   const server = new ApolloServer({
+//     schema
+//   });
+
+//   const { url } = await server.listen(9090);
+//   console.log(`🚀 Server ready at ${url}`);
+// }
+
+// startServer();
+
+import { ApolloServer } from "apollo-server-express";
+import express from "express";
+import { createServer } from "http";
+import { execute, subscribe } from "graphql";
+import { SubscriptionServer } from "subscriptions-transport-ws";
 import schema from "./graphql/schema.js";
+import { pubsub } from "./modules/blog/pubsub.js";
+import http from "http";
 
 async function startServer() {
-  const server = new ApolloServer({
-    schema
-  });
+  const app = express();
+  const httpServer = http.createServer(app);
 
-  const { url } = await server.listen(9090);
-  console.log(`🚀 Server ready at ${url}`);
+  const server = new ApolloServer({
+    schema,
+    context: () => ({ pubsub }),
+  });
+  await server.start();
+
+  server.applyMiddleware({ app });
+
+  SubscriptionServer.create(
+    {
+      schema,
+      execute,
+      subscribe,
+    },
+    {
+      server: httpServer,
+      path: server.graphqlPath,
+    }
+  );
+
+  httpServer.listen(9090, () => {
+    console.log(
+      `🚀 Server ready at http://localhost:9090${server.graphqlPath}`
+    );
+    console.log(
+      `🚀 Subscriptions ready at ws://localhost:9090${server.graphqlPath}`
+    );
+  });
 }
 
 startServer();
